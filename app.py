b5907b2301da57472a219587444f2f7efe53c79f
thesis222 <pamakdg@gmail.com>
2022-07-30 22:53:22 +0800

    update

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ class Inbox(db.Model):
     id = db.Column('student_id', db.Integer, primary_key = True)
     level = db.Column(db.String(10))
     num = db.Column(db.String(20))
-    msg = db.Column(db.String(150))
+    message = db.Column(db.String(150))
     lat = db.Column(db.Float(50))  
     lon = db.Column(db.Float(50))
 
