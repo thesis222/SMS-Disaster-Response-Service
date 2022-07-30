7c88377cc6a6cf512c2e9d1eef05a0814964a580
thesis222 <pamakdg@gmail.com>
2022-07-30 22:09:06 +0800

    update

--- app.py
+++ app.py
@@ -20,13 +20,18 @@ class Inbox(db.Model):
     lat = db.Column(db.Float(50))  
     lon = db.Column(db.Float(50))
 
+    def __repr__(self):
+        return '<Inbox %r>' % (self.level)
+
 #Create String
-def __init__(self, level, num, msg, lat, lon):
-    self.level = level
-    self.num = num
-    self.msg = msg
-    self.lat = lat
-    self.lon = lon
+# def __init__(self, level, num, msg, lat, lon):
+#     self.level = level
+#     self.num = num
+#     self.msg = msg
+#     self.lat = lat
+#     self.lon = lon
+
+
     
 @app.route('/')
 def home():
