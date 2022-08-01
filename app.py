fbf63496e3707da73d309aee53c2abeb46adf52d
thesis222 <pamakdg@gmail.com>
2022-08-01 14:14:29 +0800

    update

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ app = Flask(__name__)
 #Add Database
 app.config['SQLALCHEMY_DATABASE_URI'] ='postgres://ruvxlwwhcmievw:e0234ccf6a73ae505d15d4e6a816f1d0a386ed66b2eedee1d27cb395243e8739@ec2-54-161-255-125.compute-1.amazonaws.com:5432/ddk9t1ob7o082d'
 app.config['SECRET_KEY'] = 'smsclassification'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 #Initialize Database
 db = SQLAlchemy(app)
