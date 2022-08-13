73de436f1566e093249236bb3da2f6318746b871
thesis222 <pamakdg@gmail.com>
2022-08-13 22:00:04 +0800

    update

--- app.py
+++ app.py
@@ -1,32 +1,19 @@
+from flask import Flask, jsonify, request, render_template, url_for, session
+from flask_pymongo import PyMongo
+from bson.json_util import dumps
+from bson.objectid import ObjectId
 import pickle
-from flask import Flask, render_template, request, url_for, jsonify
-from flask_sqlalchemy import SQLAlchemy
-import sklearn, numpy
-import os
-import re
-from flask_migrate import Migrate
 
 
 app = Flask(__name__)
-#Add Database
-app.config['SQLALCHEMY_DATABASE_URI'] ='postgresql://kwsywgxyvxsepf:ce54802d0d68f7ab422f1811f1fdb43bb5afd636b4fd44321a6bd827f1a146e6@ec2-3-223-242-224.compute-1.amazonaws.com:5432/d2v8qu0kf0g8f4'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-#Initialize Database
-db = SQLAlchemy(app)
-migrate=Migrate(app, db)
+#Database Name
+app.config['MONGO_DBNAME'] = "Testdb"
+#Database URI
+app.config['MONGO_URI'] = "mongodb+srv://root:pass@cluster0.r3n21no.mongodb.net/Testdb?retryWrites=true&w=majority"
 
-class Inbox(db.Model):
-    __tablename__='inbox'
-    id = db.Column(db.Integer, primary_key = True)
-    level = db.Column(db.String(10))
-    num = db.Column(db.String(20))
-    message = db.Column(db.String(150))
-    lat = db.Column(db.Float(50))  
-    lon = db.Column(db.Float(50))
-    
-    def __repr__(self):
-        return "<Inbox %r>" % self.level
+mongo = PyMongo(app)
+
 
 @app.route('/')
 def home():
@@ -35,16 +22,14 @@ def home():
 
 @app.route('/process', methods=['POST'])
 def process():
-    content_type = request.headers.get('Content-Type')
-    if content_type !='application/json' :
-        return {'status' : 'incompatible'}
-
+    
     #JSON to form
     message = request.json['message']
-    num = request.json['num']
-    lat = request.json['lat']
-    lon = request.json['lon']
-    
+    sender = request.json ['sender']
+    receiver = request.json['receiver']
+    latitude = request.json['latitude']
+    longitude = request.json['longitude']
+
     # Load Model and DataTransform
     model, loaded_tfidfvec = load_model()
 
@@ -53,12 +38,19 @@ def process():
 
     # Predict query
     prediction = model.predict(transformed_message)[0]
-    inbox = Inbox(level= prediction, num = num, message = message, lat = lat, lon = lon )
-    db.session.add(inbox)
-    db.session.commit()
-    return {'level': prediction , 'status' : 'success'}
 
-    
+    if message and sender and receiver and latitude and longitude and request.method == 'POST':
+        id = mongo.db.test.insert_one({
+                'level':prediction,
+                'message':message,
+                'sender':sender,
+                'receiver':receiver,
+                'latitude':latitude,
+                'longitude':longitude})
+
+        return jsonify({'level': prediction , 'status' : 'success'})
+    return jsonify({'status' : 'incompatible'})
+
 # Function
 def load_model():
     model_file_name = 'model/stack_model_p.pkl'
@@ -70,11 +62,15 @@ def load_model():
     with open(data_transform_file_name, 'rb') as infile:
         loaded_tfidfvec = pickle.load(infile)
         
-    return model, loaded_tfidfvec, 
+    return model, loaded_tfidfvec 
 
-@app.route('/inbox', methods=['GET'])
-def inbox():
-    return render_template('inbox.html')
+@app.route('/table', methods = ['GET'])
+def table():
+
+    if request.method == 'GET':
+        inbox = (mongo.db.test.find())
+
+    return render_template('table.html', inbox = inbox )
 
 if __name__=='__main__':
     app.run()
